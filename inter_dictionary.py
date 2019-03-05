25b7125bbdaf7719fd4ea3efc8a9e67aa4a20341
jmsimang <jmsimang@student.wethinkcode.co.za>
2019-03-05 17:30:53 +0200

    Final user input checks implemented

--- inter_dictionary.py
+++ inter_dictionary.py
@@ -2,25 +2,30 @@ import json
 from difflib import get_close_matches
 
 
-def load_file():
+def load_file(file_path):
     """
     Function loads a json object containing words and descriptions.
     :return: The dictionary library of words and descriptions
     """
-    init_file = './dataset/data.json'
-    return json.load(open(init_file, 'r'))
+    return json.load(open(file_path, 'r'))
 
 
 def find_word(word):
     """
     Retrieves the definition of an english word.
-    :param word: word to search for
-    :return: the definition of word that is searched for
+    :param word: Word to be search for
+    :return: The definition of word that is searched for or relevant error message
     """
-    word_list = load_file()
+    # Specify file path of json dictionary
+    word_list = load_file('./dataset/data.json')
+    # Perform search and return results
     if word in word_list:
         return word_list[word]
-    elif len(get_close_matches(word, word_list.keys(), cutoff=0.8)) > 0:
+    elif word.title() in word_list:
+        return word_list[word.title()]
+    elif word.upper() in word_list:
+        return word_list[word.upper()]
+    elif len(get_close_matches(word, word_list.keys())) > 0:
         match = get_close_matches(word, word_list.keys(), cutoff=0.8)[0]
         yn = input("Did you mean '{}' instead? Enter Y if yes, or N if no: ".format(match))
         if yn.lower() == 'y':
@@ -34,13 +39,13 @@ def find_word(word):
         return "The word {} doesn't exist. Please double check it.".format(word)
 
 
-def search_word():
+def search_word(word):
     """
-
-    :return:
+    Function searches a word and prints the description of the word if found.
+    :type word: Word to search for
+    :return: None
     """
-    var = input('Enter word: ')
-    q_result = find_word(var.lower())
+    q_result = find_word(word.lower())
     if type(q_result) == list:
         for item in q_result:
             print(item)
@@ -48,4 +53,6 @@ def search_word():
         print(q_result)
 
 
-search_word()
+# Start program
+var = input('Enter word: ')
+search_word(var)
